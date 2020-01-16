cad38d2e63bf6150b3b1ceff5dd2f7e885085915
Hao Ming Liang <haoming3@cs.ualberta.ca>
2020-01-15 18:31:45 -0700

    first commit

---
+++ proxy_client.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 8001
+BUFFER_SIZE = 1024
+
+payload = 'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
+
+def connect(addr):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
+
+        full_data = s.recv(BUFFER_SIZE)
+        print(full_data.decode("ISO-8859-1"))
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+def main():
+    connect((HOST, PORT))
+
+if __name__ == "__main__":
+    main()
